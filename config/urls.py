b9ab885e7bdf58e5aaba57230a53b6a962431c69
Jose Felipe <felipe.oliveira@blitzpay.com.br>
2023-11-02 10:35:40 -0300

    refactor: update settings urls

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path("", include('app.product.urls')),
+    path("order/", include('app.order.urls')),
+    path("perfil/", include('app.perfil.urls')),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, documment_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
+    urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
-    ] + urlpatterns
+    ]
