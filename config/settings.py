db6e3e8d5327e0ede6cb59460a8aebe4bb4b443a
Jose Felipe <felipe.oliveira@blitzpay.com.br>
2023-11-07 08:22:51 -0300

    refactor: configurações

--- config/settings.py
+++ config/settings.py
@@ -28,12 +28,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     'debug_toolbar',
     'app.perfil.apps.PerfilConfig',
     'app.order.apps.OrderConfig',
     'app.product.apps.ProductConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
